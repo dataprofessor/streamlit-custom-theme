8f7d7639c6d4570455f992efbbaa174e10af8ef7
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2022-04-06 15:40:11 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,6 @@ import streamlit as st
 
 st.header('config.toml')
 
-backgroundColor = st.color_picker('Pick A Color', '#FFFFFF')
-
 with open('.streamlit/config.toml') as f:
   lines = f.readlines()
   for line in lines:
