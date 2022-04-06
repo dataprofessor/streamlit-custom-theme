a093e4c62e27fcf0d64d7c8c09f7e06607c7b894
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2022-04-06 15:34:11 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.header('config.toml')
 
-with open('.streamlit/config.toml', 'a+') as f:
+with open('.streamlit/config.toml', 'w+') as f:
   f.write('[theme]\n')
   f.write(f'primaryColor="#F63366"')
   f.write(f'backgroundColor="#FFFFFF"')
