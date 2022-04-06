bfcacbde789bcb2fc70a7ecf03480b9ee42e7672
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2022-04-06 15:39:43 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,10 +4,7 @@ st.header('config.toml')
 
 backgroundColor = st.color_picker('Pick A Color', '#FFFFFF')
 
-with open('.streamlit/config.toml', 'w+') as f:
-  f.write('[theme]\n')
-  f.write(f'primaryColor="#F63366"')
-  f.write(f'backgroundColor="{backgroundColor}"')
-  f.write(f'secondaryBackgroundColor="#F0F2F6"')
-  f.write(f'textColor="#262730"')
-  f.write(f'font="sans serif"')
+with open('.streamlit/config.toml') as f:
+  lines = f.readlines()
+  for line in lines:
+    st.write(line)
