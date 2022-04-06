36a1fccf9558dcc6c34409106d9b851239c47f9a
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2022-04-06 16:05:28 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,10 +6,10 @@ number = st.sidebar.slider('Select a number:', 0, 10, 5)
 st.write('Selected number is:', number)
 
 st.code("""
-  [theme]
-  primaryColor="#F39C12"
-  backgroundColor="#2E86C1"
-  secondaryBackgroundColor="#AED6F1"
-  textColor="#FFFFFF"
-  font="monospace"
+[theme]
+primaryColor="#F39C12"
+backgroundColor="#2E86C1"
+secondaryBackgroundColor="#AED6F1"
+textColor="#FFFFFF"
+font="monospace"
 """)
