d528627ac62dcdc61a99ea9f2d85322ea1912318
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2022-04-06 15:48:15 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,5 +3,5 @@ import streamlit as st
 st.header('config.toml')
 
 with open('.streamlit/config.toml') as f:
-  lines = f.readlines()
-  st.code(st.write(lines))
+  for line in f:
+    st.write(line)
