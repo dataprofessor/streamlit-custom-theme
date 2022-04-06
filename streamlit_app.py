bc7578a790ce9b779ca7561e3523ebf54788968b
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2022-04-06 16:04:50 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,11 @@ st.header('Contents of config.toml')
 number = st.sidebar.slider('Select a number:', 0, 10, 5)
 st.write('Selected number is:', number)
 
-with open('.streamlit/config.toml') as f:
-  for line in f:
-    st.write(line)
+st.code("""
+  [theme]
+  primaryColor="#F39C12"
+  backgroundColor="#2E86C1"
+  secondaryBackgroundColor="#AED6F1"
+  textColor="#FFFFFF"
+  font="monospace"
+""")
