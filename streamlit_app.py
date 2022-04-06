ed109d96b98b7e90d6ca530dbebc03a854121ec6
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2022-04-06 15:58:27 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.header('Contents of config.toml')
 
-number = st.slider('Test', 0, 10, 5)
+number = st.sidebar.slider('Test', 0, 10, 5)
 st.write(number)
 
 with open('.streamlit/config.toml') as f:
