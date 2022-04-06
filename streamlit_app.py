62e5390aa2e07c934642e5a5ff246c592ed5b381
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2022-04-06 16:01:00 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit as st
 st.header('Contents of config.toml')
 
 number = st.sidebar.slider('Select a number:', 0, 10, 5)
-st.write(number)
+st.write('Selected number is:', number)
 
 with open('.streamlit/config.toml') as f:
   for line in f:
