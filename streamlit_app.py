3c4a81f26d68dfd5a5435508b9364f8c2358c42c
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2022-04-06 21:34:04 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,17 +1,3 @@
 import streamlit as st
 
 st.header('Customizing the theme of Streamlit apps')
-
-st.write('Contents of the config.toml file of this app')
-
-st.code("""
-[theme]
-primaryColor="#F39C12"
-backgroundColor="#2E86C1"
-secondaryBackgroundColor="#AED6F1"
-textColor="#FFFFFF"
-font="monospace"
-""")
-
-number = st.sidebar.slider('Select a number:', 0, 10, 5)
-st.write('Selected number from slider widget is:', number)
